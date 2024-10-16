e23e04f3d80cf17e1b700b3b067376357a285d4e
Wasin Waeosri <wasin.waeosri@refinitiv.com>
2024-10-16 16:38:33 +0700

    Add runnable EMA code

--- EMAConsumerATS/Program.cs
+++ EMAConsumerATS/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace EMAConsumerATS;
 
+using System;
+using System.Threading;
+using LSEG.Ema.Access;
+
+public class AppClient : IOmmConsumerClient
+{
+    public void OnRefreshMsg(RefreshMsg refreshMsg, IOmmConsumerEvent consumerEvent)
+    {
+        Console.WriteLine(refreshMsg);
+    }
+
+    public void OnUpdateMsg(UpdateMsg updateMsg, IOmmConsumerEvent consumerEvent)
+    {
+        Console.WriteLine(updateMsg);
+    }
+
+    public void OnStatusMsg(StatusMsg statusMsg, IOmmConsumerEvent consumerEvent)
+    {
+        Console.WriteLine(statusMsg);
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadLine();
+        OmmConsumer? consumer = null;
+        try
+        {
+            AppClient appClient = new();
+            OmmConsumerConfig config = new OmmConsumerConfig().ConsumerName("Consumer_ATS").UserName("user");
+            consumer = new OmmConsumer(config);
+            consumer.RegisterClient(new RequestMsg().ServiceName("ELEKTRON_DD").Name("THB="), appClient);
+            Thread.Sleep(60000); // API calls OnRefreshMsg(), OnUpdateMsg() and OnStatusMsg()
+        }
+        catch (OmmException excp)
+        {
+            Console.WriteLine(excp.Message);
+        }
+        finally
+        {
+            consumer?.Uninitialize();
+        }
     }
 }
